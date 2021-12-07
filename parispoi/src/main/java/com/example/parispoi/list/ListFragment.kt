caf90881e7dd091ffd601b1b5f9a0f399e95668c
Duvan <duvanga@hotmail.es>
2021-12-07 14:22:42 -0500

    app con navegacion y preference

--- parispoi/src/main/java/com/example/parispoi/list/ListFragment.kt
+++ parispoi/src/main/java/com/example/parispoi/list/ListFragment.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.parispoi.databinding.FragmentListBinding
+import com.example.parispoi.main.MainActivity
 import com.example.parispoi.model.Site
 import com.example.parispoi.model.SiteItem
 import com.google.gson.Gson
@@ -16,6 +20,7 @@ class ListFragment : Fragment() {
     private lateinit var listBinding: FragmentListBinding
     private lateinit var siteAdapter: SiteAdapter
     private var listSite: ArrayList<SiteItem> = arrayListOf()
+    
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,12 +33,20 @@ class ListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        (activity as MainActivity?)?.hideIcon()
         listSite = loadMockSitesFromJson()
         siteAdapter = SiteAdapter(listSite, onItemClicked = { onSiteClicked(it) })
+
+        listBinding.sitesList.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = siteAdapter
+            setHasFixedSize(false)
+        }
 
     }
 
     private fun onSiteClicked(site: SiteItem) {
+       findNavController().navigate(ListFragmentDirections.actionListFragmentToDetailFragment(site = site))
 
     }
 
---
+++ parispoi/src/main/java/com/example/parispoi/preference/SettingsFragment.kt
@@ -0,0 +1,27 @@
+package com.example.parispoi.preference
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import androidx.preference.PreferenceFragmentCompat
+import com.example.parispoi.R
+import com.example.parispoi.main.MainActivity
+
+class SettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+        (activity as MainActivity?)?.showIcon()
+    }
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        menu.clear()
+    }
+}
